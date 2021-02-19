f7195fe38d1b3d723760e5b8d55e9a65578eb1d5
Mahi <Mahi@DESKTOP-I6BEI0Q>
2021-02-18 22:32:47 -0800

    adding exception for -ve number and delimiter added

--- src/main/java/string/calculator/StringCalculator.java
+++ src/main/java/string/calculator/StringCalculator.java
@@ -1,21 +1,38 @@
 package string.calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 	
+	public static int count = 0;
 	public static int add(String numbers) {
-		
+		count ++;
 		int returnValue=0;
 		if (numbers.isEmpty()) 
 				return 0;
 		
 		String[] numbersArray = numbers.split("[// \n\t\r.,;:!?(){]");
-			
+		List negativeNumber =new  ArrayList();
 		for(String number:numbersArray) {
+			
 			if(!number.trim().isEmpty()) {
+				int numberInt = Integer.parseInt(number.trim());
+				if(numberInt<0) {
+					negativeNumber.add(numberInt);
+				}
 				returnValue += Integer.parseInt(number);
 			}
 			
+		}		
+		
+		if(negativeNumber.size()>0) {
+			throw new RuntimeException("Negative Numbers are Not Allowed" + negativeNumber.toString());
 		}
 		return returnValue;
+	}
+	
+	public static int GetCalledCount() {
+		return count;
 	}
 }
